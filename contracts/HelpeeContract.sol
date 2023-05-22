b5cbdbc11211622bbe72adb47f2faf03e63d32c1
luishsr <luishsoares@gmail.com>
2023-05-22 18:20:45 -0300

    Initial commit: remix template blank

---
+++ contracts/HelpeeContract.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+/*
+* The Helpee smart contract implementation allowing users to:
+* 1. Post questions
+* 2. Reply to questions
+* 3. Reward answers by sending funds to the author
+*/
+contract Helpee {
+
+    // Represents an user
+    
+
+
+}
